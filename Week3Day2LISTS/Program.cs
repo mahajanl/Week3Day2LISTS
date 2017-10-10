381fa13880ab9a83908c402c13f52adb0bd8a00c
mahajanl <lakshamima@gmail.com>
2017-10-10 13:05:18 -0400

    primary commit

---
+++ Week3Day2LISTS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3Day2LISTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
